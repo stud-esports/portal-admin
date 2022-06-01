afa497b21684c44de99f9bca2f436adcc5e3da62
drmrhdt <drmrhdt@gmail.com>
2022-06-01 15:52:06 +0300

    feat(CYBER-30): add global search and export xls for all users

--- apps/portal/src/app/pages/users/users.module.ts
+++ apps/portal/src/app/pages/users/users.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
@@ -18,9 +20,11 @@ import { UsersComponent } from './users.component';
     UsersRoutingModule,
     FormsModule,
     NzModalModule,
-    NzTableModule,
+    TableModule,
     NzCheckboxModule,
     NzDatePickerModule,
+    ButtonModule,
+    InputTextModule,
   ],
 })
 export class UsersModule {}
